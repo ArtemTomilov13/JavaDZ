4e78c23d92e6d47ff18d524ae66c62576931713f
Artem <tomilow.art@yandex.ru>
2023-01-22 22:58:55 +1200

    3

--- src/Homework1/Task3.java
+++ src/Homework1/Task3.java
@@ -7,7 +7,6 @@ public class Task3 {
         int[] arr = new int[99];
         for (int i = 0; i < arr.length - 1; i++) {
             arr[i] = i + 2;
-//            System.out.print(arr[i] + " ");
             if ((arr[i] % 1 == 0) && (arr[i] % arr[i] == 0)) {
                 System.out.print(arr[i] + " ");
             }
